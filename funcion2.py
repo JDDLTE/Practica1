8adb6360e1a365f80ce18d2c05b9bd3514871dac
JDDLTE <105394704+JDDLTE@users.noreply.github.com>
2022-07-13 20:50:22 -0500

    Create funcion2.py

---
+++ funcion2.py
@@ -0,0 +1,9 @@
+def validarNumero():
+  num = int(input("ingresa un numero: "))
+  if num >=18:
+      print("numero valido")
+  else:
+       print("numero invalido")
+      
+ validarNumero()
+      
